e54bf6caff350d326f525ed4697964243b7ed554
Selivanov Pavel <selivan.at.github@gmail.com>
2019-05-04 12:52:12 +0300

    Update kibana-backup.py

--- kibana-backup.py
+++ kibana-backup.py
@@ -14,6 +14,7 @@ def backup(kibana_url, user, password):
     """Return string with json containing Kibana saved objects"""
 
     saved_object_types = ('visualization', 'dashboard', 'search', 'index-pattern', 'config', 'timelion-sheet')
+    #saved_object_types = ('index-pattern',)
     saved_objects = {}
     for obj_type in saved_object_types:
         r = requests.get(kibana_url + '/api/saved_objects/_find?type={}'.format(obj_type),
