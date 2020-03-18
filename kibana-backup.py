e93677800cfdfea88cc321a0d94bb5e9ac35d2d3
Selivanov Pavel <selivan.at.github@gmail.com>
2020-03-19 00:21:44 +0300

    Use export and import API.

--- kibana-backup.py
+++ kibana-backup.py
@@ -1,51 +1,58 @@
 #!/usr/bin/env python3
 
 # Kibana documentation:
-# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-find.html
-# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-create.html
+# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-export.html
+# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-import.html
 
-import requests
-import argparse
-import json
 import sys
+import argparse
+import requests
+
+# Error message from Kibana listing all possible saved objects types:
+# \"type\" must be one of [config, map, canvas-workpad, canvas-element, index-pattern, visualization, search, dashboard, url]
+saved_objects_types = (
+    'config', 'map', 'canvas-workpad', 'canvas-element', 'index-pattern',
+    'visualization', 'search', 'dashboard', 'url')
 
 
 def backup(kibana_url, user, password):
-    """Return string with json containing Kibana saved objects"""
-
-    saved_object_types = ('visualization', 'dashboard', 'search', 'index-pattern', 'config', 'timelion-sheet')
-    #saved_object_types = ('index-pattern',)
+    """Return string with newline-delimitered json containing Kibana saved objects"""
     saved_objects = {}
-    for obj_type in saved_object_types:
-        r = requests.get(kibana_url + '/api/saved_objects/_find?type={}'.format(obj_type),
-                         auth=(user, password), headers={'kbn-xsrf': 'true'})
-        saved_objects[obj_type] = r.json()
-    return json.dumps(saved_objects, sort_keys=True, indent=4)
+    url = kibana_url + '/api/saved_objects/_export'
+    for obj_type in saved_objects_types:
+        # print(obj_type)
+        r = requests.post(
+            url,
+            auth=(user, password),
+            headers={'Content-Type': 'application/json', 'kbn-xsrf': 'reporting'},
+            data='{ "type": "' + obj_type + '" }'
+        )
+        r.raise_for_status()  # Raises stored HTTPError, if one occurred.
+        saved_objects[obj_type] = r.text
+
+    return '\n'.join(saved_objects.values())
 
 
 def restore(kibana_url, user, password, text):
-    """Restore given json containing saved objects to Kibana"""
+    """Restore given newline-delimitered json containing saved objects to Kibana"""
 
-    saved_objects = json.loads(text)
-    for key, val in saved_objects.items():
-        for obj in val['saved_objects']:
-            obj_data = {'attributes': obj['attributes']}
-            url = kibana_url + '/api/saved_objects/{}/{}?overwrite=true'.format(key, obj['id'])
-            print('POST ' + url)
-            r = requests.post(url,
-                              auth=(user, password),
-                              headers={'Content-Type': 'application/json', 'kbn-xsrf': 'reporting'},
-                              data=json.dumps(obj_data))
-            if r.status_code != 200:
-                print(r.text)
-                r.raise_for_status()  # Raises stored HTTPError, if one occurred.
-            else:
-                print('OK')
+    url = kibana_url + '/api/saved_objects/_import?overwrite=true'
+    print('POST ' + url)
+    r = requests.post(
+        url,
+        auth=(user, password),
+        headers={'kbn-xsrf': 'reporting'},
+        files={'file': ('backup.ndjson', text)}
+    )
+
+    print(r.status_code, r.reason, '\n', r.text)
+    r.raise_for_status()  # Raises stored HTTPError, if one occurred.
 
 
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser(
-        description='Backup and restore Kibana saved objects. Writes backup to stdout and reads from stdin.')
+        description='Backup and restore Kibana saved objects. Writes backup to stdout and reads from stdin.'
+    )
     args_parser.add_argument('action', choices=['backup', 'restore'])
     args_parser.add_argument('--kibana-url', default='http://127.0.0.1:5601', help='URL to access Kibana API')
     args_parser.add_argument('--user', default='', help='Kibana user')
