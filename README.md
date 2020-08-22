d1c4bfb921b9153259914a1d6fbd76cda4dbd6f8
dougoc <eudouglastu@gmail.com>
2020-08-21 23:15:56 -0300

    Add new feature to support S3 and refactor

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ Simple backup for Kibana saved objects: config, index patterns, dashboards, save
 Uses python3 and [python-requests](https://requests.readthedocs.io/) library, which you have to install on your every host anyway, because it's awesome.
 
 ### Usage
+Backup
+```console
+kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] [--file-name FILENAME] backup
+```
 
-`kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] backup > backup.ndjson`
+Restore
+```console
+kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] [--file-name FILENAME] restore
+```
 
-`cat backup.ndjson | kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] restore`
+If you want to store backup data in S3 you need to specify environments variable AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_ID. More info [here](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html)
 
 * `backup` write backup file in newline-delimitered json format to stdout
 * `restore` restore backup from stdin
@@ -14,8 +21,11 @@ Uses python3 and [python-requests](https://requests.readthedocs.io/) library, wh
 * `--space-id` Kibana space id. If not set then the default space is used.
 * `--user` Kibana user
 * `--password` Kibana password
+* `--s3-bucket` Bucket name to save or restore a backup file
+* `--file-name` Name of a file to save backup
 
  *Note:* To use the default space you should not set `--space-id` parameter. Setting it to the default space id: `default` does not work.
+ 
 
 ### Documentation
 
--- kibana-backup.py
+++ kibana-backup.py
@@ -7,59 +7,128 @@
 import sys
 import argparse
 import requests
+import boto3
+import json
 
-# Error message from Kibana listing all possible saved objects types:
-# \"type\" must be one of [config, map, canvas-workpad, canvas-element, index-pattern, visualization, search, dashboard, url]
+# Error message from Kibana listing all possible saved objects types: \"type\" must be one of [config, map,
+# canvas-workpad, canvas-element, index-pattern, visualization, search, dashboard, url]
 saved_objects_types = (
     'config', 'map', 'canvas-workpad', 'canvas-element', 'index-pattern',
     'visualization', 'search', 'dashboard', 'url')
 
 
-def backup(kibana_url, space_id, user, password):
-    """Return string with newline-delimitered json containing Kibana saved objects"""
-    saved_objects = {}
-    if len(space_id):
-        url = kibana_url + '/s/' + space_id + '/api/saved_objects/_export'
-    else:
-        url = kibana_url + '/api/saved_objects/_export'
-    for obj_type in saved_objects_types:
-        # print(obj_type)
+class Amazon:
+    def __init__(self):
+        self.s3 = self.s3_conn()
+
+    def s3_conn(self):
+        try:
+            s3 = boto3.client('s3')
+        except Exception as err:
+            raise Exception(f'Failed in S3 connection, environments to aws are defined?: {err}')
+        return s3
+
+    def upload_backup(self, fileobj, bucket, file_name):
+        try:
+            self.s3.put_object(Body=fileobj, Bucket=bucket, Key=file_name)
+        except Exception as err:
+            raise Exception(f'Failed in upload file: {err}')
+
+    def get_backup(self, bucket, file_name, tmp_location):
+        try:
+            with open(tmp_location, 'wb') as f:
+                self.s3.download_fileobj(bucket, file_name, f)
+        except Exception as err:
+            raise Exception(f'Failed in get file: {err}')
+
+
+class Action:
+    def __init__(self, kibana_url, space_id, kibana_user, kibana_password, bucket, file_name):
+        self.kibana_url = kibana_url
+        self.space_id = space_id
+        self.kibana_user = kibana_user
+        self.kibana_password = kibana_password
+        self.bucket = bucket
+        self.file_name = file_name
+        if not file_name:
+            self.file_name = 'kibana-default-backup.json'
+
+    def backup(self):
+        """Return string with newline-delimitered json containing Kibana saved objects"""
+        saved_objects = {}
+        if len(self.space_id):
+            url = self.kibana_url + '/s/' + self.space_id + '/api/saved_objects/_export'
+        else:
+            url = self.kibana_url + '/api/saved_objects/_export'
+        for obj_type in saved_objects_types:
+            # print(obj_type)
+            r = requests.post(
+                url,
+                auth=(self.kibana_user, self.kibana_password),
+                headers={'Content-Type': 'application/json', 'kbn-xsrf': 'reporting'},
+                data='{ "type": "' + obj_type + '" }'
+            )
+            r.raise_for_status()  # Raises stored HTTPError, if one occurred.
+            saved_objects[obj_type] = r.text
+
+        full_file = '\n'.join(saved_objects.values())
+
+        if self.bucket:
+            aws = Amazon()
+            aws.upload_backup(full_file.encode(), self.bucket, self.file_name)
+
+            full_file = f'File uploaded with name {self.file_name} to {self.bucket}:\n {full_file}'
+
+        with open(self.file_name, 'w') as backup_file:
+            print(json.dumps(backup_file, indent=4))
+        return full_file
+
+    def restore(self):
+        """Restore given newline-delimitered json containing saved objects to Kibana"""
+
+        if len(self.space_id):
+            url = self.kibana_url + '/s/' + self.space_id + '/api/saved_objects/_import?overwrite=true'
+        else:
+            url = self.kibana_url + '/api/saved_objects/_import?overwrite=true'
+
+        file_to_post = self.file_name
+
+        if self.bucket:
+            tmp_location = f'/tmp/{self.file_name}'
+            aws = Amazon()
+            aws.get_backup(self.bucket, self.file_name, tmp_location)
+            file_to_post = tmp_location
+
+        if sys.stdin.readlines():
+            text = ''.join(sys.stdin.readlines())
+            file_to_post = (self.file_name, text)
+
+        print('POST ' + url)
         r = requests.post(
             url,
-            auth=(user, password),
-            headers={'Content-Type': 'application/json', 'kbn-xsrf': 'reporting'},
-            data='{ "type": "' + obj_type + '" }'
+            auth=(self.kibana_user, self.kibana_password),
+            headers={'kbn-xsrf': 'reporting'},
+            files={'file': file_to_post}
         )
+
+        print(r.status_code, r.reason, '\n', r.text)
         r.raise_for_status()  # Raises stored HTTPError, if one occurred.
-        saved_objects[obj_type] = r.text
-
-    return '\n'.join(saved_objects.values())
 
 
-def restore(kibana_url, space_id, user, password, text):
-    """Restore given newline-delimitered json containing saved objects to Kibana"""
-
-    if len(space_id):
-        url = kibana_url + '/s/' + space_id + '/api/saved_objects/_import?overwrite=true'
-    else:
-        url = kibana_url + '/api/saved_objects/_import?overwrite=true'
-    print('POST ' + url)
-    r = requests.post(
-        url,
-        auth=(user, password),
-        headers={'kbn-xsrf': 'reporting'},
-        files={'file': ('backup.ndjson', text)}
-    )
-
-    print(r.status_code, r.reason, '\n', r.text)
-    r.raise_for_status()  # Raises stored HTTPError, if one occurred.
-
-
-if __name__ == '__main__':
+def default_parser():
     args_parser = argparse.ArgumentParser(
-        description='Backup and restore Kibana saved objects. Writes backup to stdout and reads from stdin.'
+        description='Backup and restore Kibana saved objects. Writes backup to a file local or in s3 and reads from '
+                    'then. '
     )
-    args_parser.add_argument('action', choices=['backup', 'restore'])
+    subparser = args_parser.add_subparsers(help="Using backup or restore", dest='command')
+
+    subparser.add_parser('backup')
+
+    subparser.add_parser('restore')
+
+    # Default options
+    args_parser.add_argument('--s3-bucket', default='', help='S3 bucket name')
+    args_parser.add_argument('--file-name', default='', help='File name to store or to be restor')
     args_parser.add_argument('--kibana-url', default='http://127.0.0.1:5601', help='URL to access Kibana API')
     args_parser.add_argument('--space-id', default='',
                              help='Kibana space id. If not set then the default space is used.')
@@ -67,7 +136,15 @@ if __name__ == '__main__':
     args_parser.add_argument('--password', default='', help='Kibana password')
     args = args_parser.parse_args()
 
-    if args.action == 'backup':
-        print(backup(args.kibana_url, args.space_id, args.user, args.password))
-    elif args.action == 'restore':
-        restore(args.kibana_url, args.space_id, args.user, args.password, ''.join(sys.stdin.readlines()))
+    return args
+
+
+if __name__ == '__main__':
+    args = default_parser()
+    act = Action(args.kibana_url, args.space_id, args.user, args.password, args.s3_bucket, args.file_name)
+
+    if args.command == 'backup':
+        response = act.backup()
+        print(response)
+    elif args.command == 'restore':
+        act.restore()
