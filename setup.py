52ec43f6cb25bdf4f4a62576e1b6499741689d90
Pavel Selivanov <selivan.at.github@gmail.com>
2020-11-15 12:25:17 +0300

    set author name and email

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     long_description=open("README.md", 'r').read(),
     long_description_content_type='text/markdown',
     # twine requires author_email if author is set, but I don't like spam so homepage is enough
-    # author='Pavel Selivanov (selivan@github)',
-    # author_email='',
+    author='Pavel Selivanov github.com/selivan',
+    author_email='selivan.at.github@gmail-REMOVE-ANTI-SPAM.com',
     python_requires='>=3.5',
     install_requires=[
         'requests'
